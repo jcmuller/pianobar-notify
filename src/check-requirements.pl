e51baa27df18bb2d7d51a2e3919ee4ac59dc9758
Juan C. Muller <jcmuller@gmail.com>
2010-11-23 15:57:02 -0500

    Remove options and libraries not needed.

---
+++ src/check-requirements.pl
@@ -0,0 +1,48 @@
+#!/usr/bin/perl 
+
+# {{{ POD
+
+=head1 AUTHOR
+
+(c) 2010 Juan C. Muller E<lt>jcmuller@gmail.comE<gt>. All rights reserved.
+
+=head1 DESCRIPTION
+
+Check requirements for mpd-notify
+
+=head1 VERSION
+
+v0.1
+
+=head1 LICENSE
+
+This program is free software; you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation; either version 2 of the License, or (at your option) any later
+version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with
+this program; if not, write to the Free Software Foundation, Inc., 51 Franklin
+Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+=cut
+# }}}
+
+use strict;
+use warnings;
+use Test::More tests => 6;
+
+BEGIN {
+	use_ok('common::sense');
+	use_ok('Desktop::Notify');
+	use_ok('Getopt::Long');
+	use_ok('Pod::Usage');
+};
+
+
+# vim:set foldmethod=marker noexpandtab sw=4 ts=4:
+
