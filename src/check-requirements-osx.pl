c92eede87ea3192f101896586132c6b908cbf7ac
Juan C. Müller <jcmuller@gmail.com>
2011-11-11 09:44:02 -0500

    Modify for Growl 1.3 (using GNTP)

---
+++ src/check-requirements-osx.pl
@@ -0,0 +1,51 @@
+#!/usr/bin/perl
+
+# {{{ POD
+
+=head1 AUTHOR
+
+(c) 2010 Juan C. Muller E<lt>jcmuller@gmail.comE<gt>. All rights reserved.
+
+=head1 DESCRIPTION
+
+Check requirements for pianobar-notify
+
+=head1 VERSION
+
+v0.1
+
+=head1 LICENSE
+
+This program is free software; you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation; either version 2 of the License, or (at your option) any later
+version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with
+this program; if not, write to the Free Software Foundation, Inc., 51 Franklin
+Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+=cut
+# }}}
+
+use strict;
+use warnings;
+use Test::More tests => 7;
+
+BEGIN {
+	use_ok('common::sense');
+	use_ok('Getopt::Long');
+	use_ok('LWP::Simple');
+	use_ok('Pod::Usage');
+	use_ok('JSON');
+	use_ok('HTML::Entities');
+	use_ok('Growl::GNTP');
+};
+
+
+# vim:set foldmethod=marker noexpandtab sw=4 ts=4:
+
